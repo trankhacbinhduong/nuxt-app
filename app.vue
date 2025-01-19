a31d1008e4fed72bfc4f52537793b8b2ee3fb555
trankhacbinhduong <trankhacbinhduong@gmail.com>
2025-01-19 11:02:16 +0700

    :thumbsup: Implement learning dashboard layout

--- app.vue
+++ app.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="learning-dashboard">
+    <AppHeader />
   </div>
 </template>
+
+<style scoped lang="scss">
+.learning-dashboard {
+  display: grid;
+  grid-template-columns: 1fr 300px;
+  height: 100vh;
+  gap: var(--space-16);
+  background: linear-gradient(180deg, var(--color-background) 50.06%, #A9DAEC 100%);
+  padding: 84px 64px 0px 64px;
+  overflow: hidden;
+
+  @include media-down(md) {
+    grid-template-columns: 1fr;
+    padding: 84px 32px 0px 32px;
+  }
+
+  @include media-down(sm) {
+    padding: 84px 16px 0px 16px;
+  }
+}
+</style>
---
+++ components/AppHeader.vue
@@ -0,0 +1,54 @@
+<template>
+  <header class="app-header">
+    <div class="brand-info">
+      <img class="menu-btn" src="~/assets/images/menu.svg" alt="Menu">
+      <div class="logo">
+        <img src="~/assets/images/logo.svg" alt="PREP">
+      </div>
+    </div>
+    <div class="user-info">
+      <img
+        class="avatar"
+        width="40"
+        height="40"
+        src="~/assets/images/default-avatar.jpeg"
+        alt="User Avatar"
+      >
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.app-header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: var(--space-64);
+  background: var(--color-background);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 var(--space-20);
+  padding: var(--space-12) var(--space-32);
+  border-bottom: 2px solid #FFFFFF26;
+  gap: 0px;
+  z-index: 10;
+
+  > .brand-info {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 180px;
+
+    @include media-down(sm) {
+      width: 150px;
+    }
+  }
+
+  > .user-info > .avatar {
+    border-radius: var(--border-radius-round);
+    border: 1px solid var(--color-white);
+  }
+}
+</style>
